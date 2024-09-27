f53704b7358e28fefcc3b2f042488e2a9b1a71a1
Vladyslav Kopylets <vladyslav.kopylets@unibrix.com>
2024-09-27 16:36:34 +0300

    change theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,16 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
+	"catppuccin/nvim",
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		local catppuccin = require("catppuccin")
+		catppuccin.setup({
+			flavour = "auto", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "macchiato",
+			},
+		})
+		-- load the colorscheme here
+		vim.cmd([[colorscheme catppuccin]])
+	end,
 }
