6065b4295f8cbb102fd1873721b4065f5d015860
deand0n <57573532+deand0n@users.noreply.github.com>
2024-10-14 22:31:32 +0300

    feat: copy some defaults from LazyVim

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,9 +1,15 @@
 return {
 	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
+	cmd = { "TodoTrouble", "TodoTelescope" },
+	event = "VeryLazy",
+	opts = {},
+    -- stylua: ignore
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+      { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>tt", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      { "<leader>tT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    }    ,
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,1 +1,40 @@
-return { "folke/which-key.nvim" }
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		spec = {
+			{
+				mode = { "n", "v" },
+				{ "<leader>c", group = "code" },
+				{ "<leader>g", group = "git" },
+				{ "<leader>gh", group = "hunks" },
+				{ "<leader>t", group = "search" },
+				{ "<leader>x", group = "diagnostics/quickfix" },
+				{ "g", group = "goto" },
+				{ "gs", group = "surround" },
+				{ "<leader>f", group = "file/find" },
+				{ "<leader><tab>", group = "tabs" },
+				{ "[", group = "prev" },
+				{ "]", group = "next" },
+				{ "z", group = "fold" },
+				{
+					"<leader>b",
+					group = "buffer",
+					expand = function()
+						return require("which-key.extras").expand.buf()
+					end,
+				},
+				{
+					"<leader>w",
+					group = "windows",
+					proxy = "<c-w>",
+					expand = function()
+						return require("which-key.extras").expand.win()
+					end,
+				},
+				-- better descriptions
+				{ "gx", desc = "Open with system app" },
+			},
+		},
+	},
+}
