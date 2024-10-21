d8f8be8deeb2555ebfeffb0805645257bb2d9fa7
Vladyslav Kopylets <57573532+deand0n@users.noreply.github.com>
2024-10-21 16:25:47 +0300

    fix: add missing function def in nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -14,6 +14,11 @@ return {
 		local cmp = require("cmp")
 		vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
+		local has_words_before = function()
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
 		cmp.setup({
 			snippet = {
 				expand = function(args)
