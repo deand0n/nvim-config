0b01d3da2cf036f7196d3134f745a483b5f9b96f
deand0n <57573532+deand0n@users.noreply.github.com>
2025-01-01 12:28:36 +0200

    some noice changes

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,6 +2,14 @@ return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
 	opts = {
+		messages = {
+			enabled = true, -- enables the Noice messages UI
+			view = "mini", -- default view for messages
+			view_error = "notify", -- view for errors
+			view_warn = "notify", -- view for warnings
+			view_history = "messages", -- view for :messages
+			view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+		},
 		-- add any options here
 	},
 	dependencies = {
